49d0b957f61f193939a28538e770de62c7772bf3
Carlos M <53409854+Cema2019@users.noreply.github.com>
2025-10-21 14:13:44 -0600

    chore: Configure application root with MUI theme and CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Define a simple theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4CAF50", 
+    },
+    secondary: {
+      main: "#FF9800", 
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Reset CSS styles MUI */}
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
