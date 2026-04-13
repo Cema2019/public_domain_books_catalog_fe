ea505f60e414bbe155dfb3a601bc244b1b336637
Carlos M <53409854+Cema2019@users.noreply.github.com>
2026-04-13 16:24:52 -0600

    feat(app): add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // Define a simple theme
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#4CAF50", 
+      main: "#4CAF50",
     },
     secondary: {
-      main: "#FF9800", 
+      main: "#FF9800",
     },
   },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Reset CSS styles MUI */}
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> {/* Reset CSS styles MUI */}
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
